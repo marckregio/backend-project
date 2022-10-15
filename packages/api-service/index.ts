a425b7ea19b53476aef6795c13cc75b2250e2398
marckregio <marckregio@gmail.com>
2022-10-15 09:22:42 +0800

    apply clustering

--- packages/api-service/index.ts
+++ packages/api-service/index.ts
@@ -8,37 +8,72 @@ import morgan from "morgan";
 import { RegisterRoutes } from "./routes/routes";
 import { AuthenticateRoutes } from "./routes/authenticate";
 
-const app: express.Application = express();
-const server: http.Server = http.createServer(app);
-const port: string | number = 4001;
+import cluster from "cluster";
+import totalCPUs from "os";
 
-app.use(cors());
-app.use(morgan("tiny"));
-app.use(express.static("public"));
-app.use(bodyParser.json({ limit: "100mb" }));
-app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(undefined, {
-    swaggerOptions: {
-      url: "/swagger.json",
-      explorer: true,
-      validatorUrl: null
-    },
-    customSiteTitle: "API Documentation",
-    customfavIcon: "/favicon.ico",
-    customCssUrl: "/swag.css",
-  }),
-);
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.status(200).send("Nothing will see here. But it works!");
-});
+//clustering sample
+if (cluster.isMaster) {
+  console.log(`Number of CPUs is ${totalCPUs.cpus().length}`);
+  console.log(`Master ${process.pid} is running`);
 
-server.listen(port, () => {
-	new RegisterRoutes(app);
-  new AuthenticateRoutes(app);
-});
+  // Fork workers.
+  let core = 4 //safe definition
+  for (let i = 0; i < core; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
+  const app: express.Application = express();
+  const server: http.Server = http.createServer(app);
+  const port: string | number = 4001;
+
+  app.use(cors());
+  app.use(morgan("tiny"));
+  app.use(express.static("public"));
+  app.use(bodyParser.json({ limit: "100mb" }));
+  app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+  app.use(express.json({ limit: "100mb" }));
+  app.use(express.urlencoded({ limit: "100mb", extended: true }));
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+      swaggerOptions: {
+        url: "/swagger.json",
+        explorer: true,
+        validatorUrl: null
+      },
+      customSiteTitle: "API Documentation",
+      customfavIcon: "/favicon.ico",
+      customCssUrl: "/swag.css",
+    }),
+  );
+  app.get("/", (req: express.Request, res: express.Response) => {
+    res.status(200).send("Nothing will see here. But it works!");
+  });
+  
+  app.get("/count/:n", function (req, res) {
+    let n = parseInt(req.params.n);
+    let count = 0;
+  
+    if (n > 5000000000) n = 5000000000;
+  
+    for (let i = 0; i <= n; i++) {
+      count += i;
+    }
+  
+    res.send(`Final count is ${count}`);
+  });
+  
+  server.listen(port, () => {
+    new RegisterRoutes(app);
+    new AuthenticateRoutes(app);
+  });
+}
